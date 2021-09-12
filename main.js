69bccec11d43edbb6de7a9be11ab53e4477fc083
asdcf35 <66645620+asdcf35@users.noreply.github.com>
2021-09-12 15:44:12 -0400

    cio

--- main.js
+++ main.js
@@ -8,26 +8,26 @@ menu_list_array = [
                   ];
 
 function getmenu(){
-var htmldata;
-menu_list_array.sort();
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+ menu_list_array[i] + '<br>'
-        }
-        document.getElementById("display_menu").innerHTML = htmldata;
-        //give the appropriate id name as display_menu  
+    var htmldata;
+    // menu_list_array.sort();
+            for(var i=0;i<menu_list_array.length;i++){
+                htmldata=htmldata+ menu_list_array[i] + '<br>'
+            }
+            document.getElementById("display_menu").innerHTML = htmldata;
+            //give the appropriate id name as display_menu  
 }
 
 function add_item(){
-var htmldata;
-var item=document.getElementById("add_item").value;
-        // use the sort function as - menu_list_array.sort();
-        menu_list_array.sort();
-        htmldata=""
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
-        }
-         document.getElementById("display_addedmenu").innerHTML = htmldata;		
-      }
+        var htmldata;
+        var item=document.getElementById("add_item").value;
+                // use the sort function as - menu_list_array.sort();
+                menu_list_array.sort();
+                htmldata=""
+                for(var i=0;i<menu_list_array.length;i++){
+                    htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+                }
+                document.getElementById("display_addedmenu").innerHTML = htmldata;		
+            }
 
 function add_top(){
     var item=document.getElementById("add_item").value;
