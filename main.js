afa3c4408a1a1eb3ed01093b68433da14c3d0e44
asdcf35 <66645620+asdcf35@users.noreply.github.com>
2021-09-12 15:46:25 -0400

    com'

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ menu_list_array = [
 
 function getmenu(){
     var htmldata;
-    // menu_list_array.sort();
+    menu_list_array.sort();
             for(var i=0;i<menu_list_array.length;i++){
                 htmldata=htmldata+ menu_list_array[i] + '<br>'
             }
