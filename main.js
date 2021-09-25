978dff3c82499e5cf42e329aa69a3496639cdba1
asdcf35 <66645620+asdcf35@users.noreply.github.com>
2021-09-24 20:05:42 -0400

    Final Commit

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 menu_list_array = [
-                    "Veg Margherita Pizza",
-                    "Neapolitan Pizza",
-                    "Chicago Pizza",
-                    "New York-Style Pizza",
-                    "Sicilian Pizza",
-                    "Greek Pizza",
+                    "Veg Margherita Pizza<br>",
+                    "Neapolitan Pizza<br>",
+                    "Chicago Pizza<br>",
+                    "New York-Style Pizza<br>",
+                    "Sicilian Pizza<br>",
+                    "Greek Pizza<br>",
                   ];
 
 function getmenu(){
-    var htmldata;
-    var imgtags = '<img src="https://www.pikpng.com/pngl/m/54-548943_pizza-clipart-png-pizza-clipart-png-clip-art.png" alt="Pizza Clipart Png Pizza Clipart Png - Clip Art Pizza Transparent Background@pikpng.com">'
-    menu_list_array.sort();
-            menu_list_array.array.forEach(element => {
-                htmldata = element + '<br>'
-            });
+    var htmldata = "";
+  menu_list_array.sort();
+            for(var i=0;i<menu_list_array.length;i++){
+                    htmldata+=menu_list_array[i]+'<br>';
+		}
+	console.log(htmldata);
             document.getElementById("display_menu").innerHTML = htmldata;
             //give the appropriate id name as display_menu  
 }
 
 function add_item(){
         var htmldata;
-        var imgtags = '<img src="https://www.pikpng.com/pngl/m/54-548943_pizza-clipart-png-pizza-clipart-png-clip-art.png" alt="Pizza Clipart Png Pizza Clipart Png - Clip Art Pizza Transparent Background@pikpng.com">'
+        var imgtags = '<img src="https://www.pikpng.com/pngl/m/54-548943_pizza-clipart-png-pizza-clipart-png-clip-art.png" alt="Pizza Clipart Png Pizza Clipart Png - Clip Art Pizza Transparent Background@pikpng.com" height="45" width="45">'
         var item=document.getElementById("add_item").value;
                 // use the sort function as - menu_list_array.sort();
                 menu_list_array.sort();
                 htmldata=""
                 for(var i=0;i<menu_list_array.length;i++){
-                    htmldata=htmldata+imgtags+menu_list_array[i]+'<br>';
+                    htmldata+=imgtags+menu_list_array[i]+'<br>';
                 }
                 document.getElementById("display_addedmenu").innerHTML = htmldata;		
             }
